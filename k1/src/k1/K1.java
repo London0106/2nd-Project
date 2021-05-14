378f48ad4b9b4a1148e5d0354cd30df1d4895dd7
London0106 <london.jts@gmail.com>
2021-05-14 22:28:37 +0700

    Fourth Commit

--- k1/src/k1/K1.java
+++ k1/src/k1/K1.java
@@ -15,6 +15,8 @@ public class K1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+//        Suhu mula-mula suatu ruang penyimpanan ikan adalah 26°C. Ruangan tersebut akan digunakan
+//untuk menyimpan ikan, sehingga suhu diturunkan menjadi 4°C di bawah nol. Berapa besar perubahan suhu di dalam ruangan tersebut ?
         // TODO code application logic here
     }
     
