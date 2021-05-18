aed5f386baad30091b6fc310aaddfbebbc6062a1
London0106 <london.jts@gmail.com>
2021-05-18 19:12:03 +0700

    second commit

---
+++ London's Project/src/london/s/project/LondonSProject.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package london.s.project;
+
+/**
+ *
+ * @author London
+ */
+public class LondonSProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int multiplication = 5 * 9 ;
+        System.out.println("5 x 9 = "+multiplication);
+        
+        int division = 6 / 3 ;
+        System.out.println("6 / 3 = "+division);
+        
+        int plus = 10 + 25 ;
+        System.out.println("10 + 25 = "+plus);
+        
+        int minus = 70 - 27 ;
+        System.out.println("70 - 27 = "+minus);
+        
+        System.out.println("this are the types of multiples") ;
+        
+        System.out.println("by = London");
+        
+        
+               
+              
+        
+   
+              
+                
+    }
+    
+}
